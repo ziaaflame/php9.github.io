7c705b7961c464a553006cbea1549ada6bd60892
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-27 11:48:43 +0600

    php9

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $array = [
+        'name' => [
+            'first_name' => 'Md Zia',
+            'last_name' => 'Uddin'
+        ]
+        'address' => [
+            'street' => [
+                'vill' => 'west mekhal',
+                'road' => 'mekhal',
+                'p.s' => 'hathazari',
+            ]
+        'phone' => [
+            'country_prefix' => '+880',
+            'country_mobilie' => '1819400400',
+            ]
+        ]
+    ]
+?>
